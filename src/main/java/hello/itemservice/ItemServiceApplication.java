6524c8100afb9086c2c341ba0233613b1cbffe47
kevin <dongohkang@gangdong-oui-MacBookPro.local>
2026-04-22 02:37:57 +0900

    Spring Data JPA basic study

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 
 
-@Import(JpaConfig.class)
+@Import(SpringDataJpaConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
